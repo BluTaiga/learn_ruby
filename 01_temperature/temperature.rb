0d091b265acabb2d32b6eeb1c208507085072025
BluTaiga <tjbermant@gmail.com>
2017-05-03 21:24:36 -0700

    solutions to problems

--- 01_temperature/temperature.rb
+++ 01_temperature/temperature.rb
@@ -1,1 +1,10 @@
 #write your code here
+# Deduct 32, then multiply by 5, then divide by 9
+
+def ftoc(temp)
+    ((temp - 32.0) * 5.0) / 9.0
+end
+
+def ctof(temp)
+    ((temp * 9.0) / 5.0) + 32.0
+end
--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,18 @@
 #write your code here
+def add(num1,num2)
+    (num1) + (num2)
+end
+
+def subtract(num1,num2)
+    if num1 > num2
+        (num1) - (num2)
+    else
+        (num2) - (num1)
+    end
+end
+
+def sum(ary)
+    ary.each |int1, int2|
+    int1 + int2
+    end
+end
