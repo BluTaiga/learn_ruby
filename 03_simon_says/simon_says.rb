87086b6bfd64e410be245598837ceb412ab717ff
Thomas Bermant <tjbermant@gmail.com>
2017-05-05 22:45:33 -0700

    start on simon

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -1,1 +1,19 @@
 #write your code here
+def echo(word)
+  "#{word}"
+end
+
+def shout(word)
+  "#{word.upcase}"
+end
+
+def repeat(word, num=2)
+  arr = Array.new(num) { word }
+  arr.join(" ")
+end
+
+def start_of_word(word, place=1)
+  place -= 1
+  arr = word.split(//)
+  arr.take()
+end
