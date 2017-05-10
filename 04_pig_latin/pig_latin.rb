3481777db31d08700740f2150f0cab6a182b0d3e
BluTaiga <tjbermant@gmail.com>
2017-05-09 22:32:20 -0700

    continued pig latin work

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -4,10 +4,17 @@ require 'pry'
 def translate(word)
   vowels = ["a","e","i","o","u"]
   the_word = word.split("")
-  if the_word[0] == vowels 
+  
+  if vowels.each { |x| x == the_word[0] }
     the_word << "ay"
     the_word.join
   else
+    # Remove the first element of the array and assign to a variable
+    first = the_word.shift
+    # Insert this string into the array at the end.
+    the_word << first
+    # append "ay"
+    the_word << "ay"
     the_word.join
   end
 end
