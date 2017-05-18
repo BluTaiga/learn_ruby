d80677adac551c2235090462e9c8be50870049ef
BluTaiga <tjbermant@gmail.com>
2017-05-18 16:24:27 -0700

    final solution for pig latin

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,20 +1,29 @@
 #write your code here
 require 'pry'
 
-def translate(word)
-  vowels = ["a","e","i","o","u"]
-  the_word = word.split("")
+# def translate(word)
+#   vowels = ["a","e","i","o","u"]
+#   the_word = word.split("")
   
-  if vowels.each { |x| x == the_word[0] }
-    the_word << "ay"
-    the_word.join
-  else
-    # Remove the first element of the array and assign to a variable
-    first = the_word.shift
-    # Insert this string into the array at the end.
-    the_word << first
-    # append "ay"
-    the_word << "ay"
-    the_word.join
-  end
+#   if vowels.any? { |x| x == the_word[0] }
+#     the_word << "ay"
+#     the_word.join
+#   else
+#     first = the_word.shift
+#     the_word << "#{first}" + "ay"
+#     the_word.join
+#   end
+# end
+
+
+#find the index of the first  vowels
+def translate(word)
+  the_word = word.split("")
+  vowel_index = the_word.index(/[aeiou]/)
+  #take the characters off the word up to the index number.
+  first = the_word.shift(vowel_index)
+  #shovel them at the back of the word and apptend "ay"
+  the_word << "#{first}" + "ay"
+  the_word.join
 end
+
