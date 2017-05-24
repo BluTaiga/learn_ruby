03c368d3dc514e456bd8066aee76f934ba707c34
BluTaiga <tjbermant@gmail.com>
2017-05-24 13:59:48 -0700

    Adds completee solution to 05

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,18 @@
+require 'pry'
+
 class Book
-# write your code here
+  attr_writer :title
+  def title
+    do_not_cap = ["and","in","the","of","a","an"]
+    words = @title.split(" ")
+    words.map! do |word| 
+      if do_not_cap.include?(word) 
+        word
+      else
+        word.capitalize
+      end
+    end
+    words[0] = words[0].capitalize
+    words.join(" ")
+  end
 end
