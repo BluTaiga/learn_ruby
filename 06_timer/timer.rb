6913cc09ce65a37b0562d6a4e8bf3901645184d4
BluTaiga <tjbermant@gmail.com>
2017-05-25 13:28:19 -0700

    Finishes the project

--- 06_timer/timer.rb
+++ 06_timer/timer.rb
@@ -1,3 +1,39 @@
+require 'pry'
+
 class Timer
-  #write your code here
+  attr_writer :seconds
+  def initialize 
+    @seconds = 0
+  end
+
+  def seconds
+    @total_seconds = @seconds
+  end
+
+  def time_string
+    "#{hours.timefy}:#{minutes.timefy}:#{secs.timefy}"
+  end
+
+ #### Calculations ####
+  def hours
+    seconds / 3600
+  end
+
+  def minutes
+    if seconds < 60
+      0
+    else
+      remainder = (seconds - (hours * 3600)) / 60
+    end
+  end
+
+  def secs
+    seconds - (minutes * 60) - (hours * 3600)
+  end
+end
+
+class Integer
+  def timefy
+    ("%02d" % self)
+  end
 end
